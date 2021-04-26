e8d87d3009b601cb2392284111e79ded1597d4d4
Stefan <stefan.mi@coinis.com>
2021-04-26 10:04:53 +0200

    establish appsync connection

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,31 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import Client from "aws-appsync";
+import { ApolloProvider } from "react-apollo";
+import { Rehydrated } from "aws-appsync-react";
+import AppSync from "./AppSync";
+
+const client = new Client({
+  url: AppSync.aws_appsync_graphqlEndpoint,
+  region: AppSync.aws_appsync_region,
+  auth: {
+    type: AppSync.aws_appsync_authenticationType,
+    apiKey: AppSync.aws_appsync_apiKey,
+  },
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <WithProvider />
   </BrowserRouter>,
   document.getElementById("root")
 );
