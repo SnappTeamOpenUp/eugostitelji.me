f3e12811bfc66b211189188e792a1b212389c8d8
stemili <stefanmilikic999@gmail.com>
2021-05-03 13:27:51 +0200

    add attributes to dynamodb table, adjust create function, update graphql accordingly

--- services/core/functions/createServiceProvider.js
+++ services/core/functions/createServiceProvider.js
@@ -3,14 +3,18 @@ import dynamoDb from "../libs/dynamodb-lib";
 import { v4 as uuidv4 } from "uuid";
 
 export const main = handler(async (event) => {
-  // const { id } = event.arguments;
+  const { title, logoImg, workingHours, coverImg } = event.arguments;
 
   const params = {
     TableName: process.env.searchTable,
     Item: {
-      userId: "1", // The id of the author
       pib: uuidv4(), // A unique uuid
-      // pib: id, // Parsed from request body
+      userId: "1", // The id of the author
+      dateCreated: Date.now(),
+      title,
+      logoImg,
+      workingHours,
+      coverImg,
     },
   };
   await dynamoDb.put(params);
