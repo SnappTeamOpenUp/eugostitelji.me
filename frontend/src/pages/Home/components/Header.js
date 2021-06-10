4b7d5d314fff205baed305a8af3fbed4d4bea315
stemili <stefan.mi@coinis.com>
2021-06-10 19:57:22 +0200

    add service provider api call, initialize service provider page, implement search dropdown logic on home-header

--- frontend/src/pages/Home/components/Header.js
+++ frontend/src/pages/Home/components/Header.js
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import { searchAlgolia } from "../../../services/search";
 
 export const Header = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [autocompleteData, setAutocompleteData] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
   const handleSearch = async (term) => {
-    setLoading(true);
+    // setLoading(true);
     try {
       const res = await searchAlgolia(term, {});
-
-    }catch(e){
+      setAutocompleteData(res.serviceProviders);
+    } catch (e) {
       console.log(e);
-    }finally{
-      setLoading(false);
+    } finally {
+      // setLoading(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      handleSearch(searchTerm);
+    }
+  }, [searchTerm]);
 
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-2xl mx-auto sm:max-w-xl md:max-w-2xl">
-        <div className="text-center">
+        <div className="text-center relative">
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             <div>
               <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
@@ -57,17 +65,36 @@ export const Header = () => {
               recipe is a story that ends with a good meal
             </h2>
           </div>
-          <div className="flex flex-col items-center w-full mb-20 md:flex-row md:px-16">
+          <div className="flex flex-col items-center w-full  md:flex-row md:px-16">
             <input
               placeholder="Find restaurants, delivery, takeout..."
               value={searchTerm}
-              onChange={e => setSearchTerm(e.target.value)}
+              onChange={(e) => setSearchTerm(e.target.value)}
               type="text"
               className="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
             />
-            <button onClick={() => handleSearch(searchTerm)} className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">
-              {loading ? 'Searching...' : 'Search'} 
+            <button
+              onClick={() => handleSearch(searchTerm)}
+              className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+            >
+              Search
             </button>
+          </div>
+          <div className="flex flex-col absolute w-full">
+            {autocompleteData &&
+              autocompleteData.length > 0 &&
+              autocompleteData.map((item) => (
+                <Link to={`/service-provider/${item.id}`} key={item.id}>
+                  <div className="flex flex-row h-16 justify-between items-center	border-b p-2 hover:bg-gray-100">
+                    <span>{item.title}</span>
+                    <img
+                      className="h-full"
+                      src={item.logoImg}
+                      alt="logo_image"
+                    />
+                  </div>
+                </Link>
+              ))}
           </div>
         </div>
       </div>
--- frontend/src/pages/ServiceProvider/index.js
+++ frontend/src/pages/ServiceProvider/index.js
@@ -1,15 +1,89 @@
 import React from "react";
+import { useParams } from "react-router";
+import { gql, useQuery } from "@apollo/client";
 
 import { Navbar } from "../../components/Navbar";
-import { Footer } from "../../components/Footer";
 
-function ServiceProvider() {
+const GetServiceProvider = gql`
+  query GetServiceProvider($id: ID!) {
+    getServiceProvider(id: $id) {
+      id
+      pib
+      title
+      logoImg
+      workingHours
+      coverImg
+      city
+      country
+      address
+      latitude
+      longitude
+      website
+      tags
+    }
+  }
+`;
+
+const ServiceProvider = () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GetServiceProvider, {
+    variables: { id },
+  });
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
   return (
     <>
       <Navbar />
-      <Footer />
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
+          <div className="flex flex-col justify-center">
+            <div className="max-w-xl mb-6">
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                {data.getServiceProvider.title}
+              </h2>
+              <p className="text-base text-gray-700 md:text-lg">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae. explicabo.
+              </p>
+            </div>
+            <div className="grid gap-5 row-gap-8 sm:grid-cols-2">
+              <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
+                <div className="h-full p-5 border border-l-0 rounded-r">
+                  <h6 className="mb-2 font-semibold leading-5">
+                    I'll be sure to note that in my log
+                  </h6>
+                  <p className="text-sm text-gray-900">
+                    Lookout flogging bilge rat main sheet bilge water nipper
+                    fluke to go on account heave down.
+                  </p>
+                </div>
+              </div>
+              <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
+                <div className="h-full p-5 border border-l-0 rounded-r">
+                  <h6 className="mb-2 font-semibold leading-5">
+                    A business big enough that it could be listed
+                  </h6>
+                  <p className="text-sm text-gray-900">
+                    Those options are already baked in with this model shoot me
+                    an email clear.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img
+              className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+              src={data.getServiceProvider.coverImg}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default ServiceProvider;
